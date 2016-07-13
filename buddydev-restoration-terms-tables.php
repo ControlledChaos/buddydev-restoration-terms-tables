8b94ea6d29f94a061380f462216a4c690324ff69
Ravi Sharma <ravivats89@gmail.com>
2016-07-13 15:43:42 +0530

    code block

---
+++ buddydev-restoration-terms-tables.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Plugin Name: Buddydev Restoration Terms tables
+ *
+ */
+
+class BuddyDev_Terms_Tables_Restoration_Helper {
+
+	public function __construct() {
+
+		add_action( 'plugins_loaded', array( $this, 'restore_terms_tables' ) );
+	}
+
+	public function restore_terms_tables() {
+
+		global $wpdb;
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		$charset_collate = ! empty( $wpdb->charset ) ? "DEFAULT CHARACTER SET {$wpdb->charset}" : '';
+
+		$sites = wp_get_sites();
+		$blog_ids = wp_list_pluck( $sites, 'blog_id' );
+
+		if ( empty( $blog_ids ) ) {
+			return;
+		}
+
+		$max_index_length = 191;
+
+		foreach ( $blog_ids as $blog_id ) {
+
+			$last_process_blog_id = absint( get_option( '_last_process_blog_id' ) );
+
+			if ( $blog_id == 1 || $blog_id < $last_process_blog_id ) {
+				continue;
+			}
+
+			$blog_prefix = $wpdb->get_blog_prefix( $blog_id );
+
+			$blog_tables = "CREATE TABLE IF NOT EXISTS {$blog_prefix}terms (
+							 term_id bigint(20) unsigned NOT NULL auto_increment,
+							 name varchar(200) NOT NULL default '',
+							 slug varchar(200) NOT NULL default '',
+							 term_group bigint(10) NOT NULL default 0,
+							 PRIMARY KEY  (term_id),
+							 KEY slug (slug($max_index_length)),
+							 KEY name (name($max_index_length))
+							) $charset_collate;
+							CREATE TABLE {$blog_prefix}term_taxonomy (
+							 term_taxonomy_id bigint(20) unsigned NOT NULL auto_increment,
+							 term_id bigint(20) unsigned NOT NULL default 0,
+							 taxonomy varchar(32) NOT NULL default '',
+							 description longtext NOT NULL,
+							 parent bigint(20) unsigned NOT NULL default 0,
+							 count bigint(20) NOT NULL default 0,
+							 PRIMARY KEY  (term_taxonomy_id),
+							 UNIQUE KEY term_id_taxonomy (term_id,taxonomy),
+							 KEY taxonomy (taxonomy)
+							) $charset_collate;";
+
+			$tables = dbDelta( $blog_tables );
+
+			if ( ! empty( $tables ) ) {
+				update_option( '_last_process_blog_id', $blog_id  );
+			}
+
+		} //end of foreach;
+
+	}
+
+
+}
+new BuddyDev_Terms_Tables_Restoration_Helper();
